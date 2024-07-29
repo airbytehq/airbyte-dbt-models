4497467db99aae8bc021608227ecd15bc7b1b114
marcosmarxm <marcosmarxm@gmail.com>
2024-07-29 20:32:03 -0300

    do using vriable hard coded

--- connectors/source_zendesk_support/integration_tests/run.sh
+++ connectors/source_zendesk_support/integration_tests/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DBT_VARS=$(cat vars | sed "s/\$AB_DB/TESTING/g")
-echo "My variable is $DBT_VARS"
+# export DBT_VARS=$(cat vars | sed "s/\$AB_DB/TESTING/g")
+export DBT_VARS="{airbyte_database: dataline-integration-testing, zendesk_database: dataline-integration-testing}"
 poetry run dbt deps
-poetry run dbt run --target bigquery --full-refresh --vars "{airbyte_database: dataline-integration-testing, zendesk_database: dataline-integration-testing}"
+poetry run dbt run --target bigquery --full-refresh --vars "$DBT_VARS"
