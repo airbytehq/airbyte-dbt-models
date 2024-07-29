7d5ba0eb615c9642df1d0d58a4be9d9302181fe1
marcosmarxm <marcosmarxm@gmail.com>
2024-07-29 20:28:37 -0300

    do inline vars

--- connectors/source_zendesk_support/integration_tests/run.sh
+++ connectors/source_zendesk_support/integration_tests/run.sh
@@ -3,4 +3,4 @@
 export DBT_VARS=$(cat vars | sed "s/\$AB_DB/TESTING/g")
 echo "My variable is $DBT_VARS"
 poetry run dbt deps
-poetry run dbt run --target bigquery --full-refresh --vars $DBT_VARS
+poetry run dbt run --target bigquery --full-refresh --vars "{airbyte_database: dataline-integration-testing, zendesk_database: dataline-integration-testing}"
