ca04687cc9373aee4425ae1a6b8f389492c82a12
marcosmarxm <marcosmarxm@gmail.com>
2024-07-29 20:34:33 -0300

    use fucntion

--- connectors/source_zendesk_support/integration_tests/run.sh
+++ connectors/source_zendesk_support/integration_tests/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# export DBT_VARS=$(cat vars | sed "s/\$AB_DB/TESTING/g")
-export DBT_VARS="{airbyte_database: dataline-integration-testing, zendesk_database: dataline-integration-testing}"
+export DBT_VARS="$(cat vars | sed "s/\$AB_DB/TESTING/g")"
+# export DBT_VARS="{airbyte_database: dataline-integration-testing, zendesk_database: dataline-integration-testing}"
 poetry run dbt deps
 poetry run dbt run --target bigquery --full-refresh --vars "$DBT_VARS"
