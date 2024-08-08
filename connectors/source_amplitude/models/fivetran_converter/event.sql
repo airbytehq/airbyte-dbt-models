613823257285591b9934d0d8e1f052bacbeefbf4
Tope Folorunso <folorunsotemitope10@gmail.com>
2024-08-09 00:53:52 +0100

    add project_name, event_type_id

--- connectors/source_amplitude/models/fivetran_converter/event.sql
+++ connectors/source_amplitude/models/fivetran_converter/event.sql
@@ -1,13 +1,22 @@
 
-{% if target.type == "snowflake" %}
+{% if target.type in ["postgres", "snowflake"] %}
 
-    WITH tmp AS (
+    WITH EventTypeCTE AS (
+        SELECT 
+            id AS event_type_id, 
+            "value"
+        FROM 
+            {{ source('source_amplitude', 'event_type') }}
+    ),
+
+    Events AS (
         SELECT
             "$insert_id" AS _insert_id,
             "$schema" AS "schema",
             adid AS ad_id,
             amplitude_id,
             app,
+            app AS project_name,
             city,
             client_event_time,
             client_upload_time,
@@ -51,18 +60,40 @@
             version_name
         FROM
             {{ source('source_amplitude', 'events') }}
-        )
-    SELECT * FROM tmp
+    ),
+
+    TMP AS (
+        SELECT 
+            Events.*, 
+            EventTypeCTE.event_type_id
+        FROM 
+            Events
+        LEFT OUTER JOIN 
+            EventTypeCTE
+        ON 
+            Events.event_type = EventTypeCTE.value
+    )
+
+    SELECT * FROM TMP
 
 {% elif target.type == "bigquery" %}
 
-    WITH tmp AS (
+    WITH EventTypeCTE AS (
+        SELECT 
+            id AS event_type_id, 
+            `value`
+        FROM 
+            {{ source('source_amplitude', 'event_type') }}
+    ),
+
+    Events AS (
         SELECT
             `$insert_id` AS _insert_id,
             `$schema` AS `schema`,
             adid AS ad_id,
             amplitude_id,
             app,
+            app AS project_name,
             city,
             client_event_time,
             client_upload_time,
@@ -106,62 +137,20 @@
             version_name
         FROM
             {{ source('source_amplitude', 'events') }}
-        )
-    SELECT * FROM tmp
+    ),
 
-{% elif target.type == "postgres" %}
+    TMP AS (
+        SELECT 
+            Events.*, 
+            EventTypeCTE.event_type_id
+        FROM 
+            Events
+        LEFT OUTER JOIN 
+            EventTypeCTE
+        ON 
+            Events.event_type = EventTypeCTE.value
+    )
 
-    WITH tmp AS (
-        SELECT
-            "$insert_id" AS _insert_id,
-            "$schema" AS "schema",
-            adid AS ad_id,
-            amplitude_id,
-            app,
-            city,
-            client_event_time,
-            client_upload_time,
-            country,
-            "data",
-            device_brand,
-            device_carrier,
-            device_family,
-            device_id,
-            device_manufacturer,
-            device_model,
-            device_type,
-            dma,
-            event_id AS id,
-            event_properties,
-            event_time,
-            event_type,
-            group_properties,
-            groups AS group_types,
-            idfa,
-            ip_address,
-            is_attribution_event,
-            "language",
-            library,
-            location_lat,
-            location_lng,
-            os_name,
-            os_version,
-            paying,
-            platform,
-            processed_time,
-            region,
-            server_received_time,
-            server_upload_time,
-            session_id,
-            start_version,
-            user_creation_time,
-            user_id,
-            user_properties,
-            uuid,
-            version_name
-        FROM
-            {{ source('source_amplitude', 'events') }}
-        )
-    SELECT * FROM tmp
+    SELECT * FROM TMP
 
 {%endif%}
