797758da4d79085fb8cf31595b1db0e71d6625d6
btkcodedev <btk.codedev@gmail.com>
2024-08-11 23:37:59 +0530

    add models, fix: nullify unidentified cols

--- connectors/source_recurly/models/fivetran_converter/account_balance_history.sql
+++ connectors/source_recurly/models/fivetran_converter/account_balance_history.sql
@@ -2,28 +2,14 @@
 
     WITH tmp AS (
         SELECT
-            t.id AS account_id,
-            t.updated_at AS updated_at,
-            t.address:city::STRING AS account_city,
-            t.address:country::STRING AS account_country,
-            t.address:postal_code::STRING AS account_postal_code,
-            t.address:region::STRING AS account_region,
-            t.bill_to AS bill_to,
-            t.cc_emails AS cc_emails,
-            t.code AS code,
-            t.shipping_addresses[0]:company::STRING AS company,
-            t.created_at AS created_at,
-            t.deleted_at AS deleted_at,
-            t.shipping_addresses[0]:email::STRING AS email,
-            t.shipping_addresses[0]:first_name::STRING AS first_name,
-            ROW_NUMBER() OVER (PARTITION BY t.id ORDER BY t.updated_at DESC) = 1 AS is_most_recent_record,
-            t.tax_exempt AS is_tax_exempt,
-            t.shipping_addresses[0]:last_name::STRING AS last_name,
-            t.address:region::STRING AS state,
-            t.username AS username,
-            t.shipping_addresses[0]:vat_number::STRING AS vat_number
+            id AS account_id,
+            cast(updated_at AS {{ dbt.type_timestamp() }}) AS account_updated_at,
+            NULL AS amount, 
+            NULL AS currency, 
+            has_past_due_invoice AS past_due,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1 AS is_most_recent_record
         FROM
-            {{ source('source_recurly', 'accounts') }} t
+            {{ source('source_recurly', 'accounts') }}
     )
     SELECT * FROM tmp
     WHERE account_id IS NOT NULL
@@ -32,60 +18,32 @@
 
     WITH tmp AS (
         SELECT
-            t.id AS account_id,
-            t.updated_at AS updated_at,
-            JSON_EXTRACT_SCALAR(t.address, '$.city') AS account_city,
-            JSON_EXTRACT_SCALAR(t.address, '$.country') AS account_country,
-            JSON_EXTRACT_SCALAR(t.address, '$.postal_code') AS account_postal_code,
-            JSON_EXTRACT_SCALAR(t.address, '$.region') AS account_region,
-            t.bill_to AS bill_to,
-            t.cc_emails AS cc_emails,
-            t.code AS code,
-            JSON_EXTRACT_SCALAR(t.shipping_addresses[SAFE_OFFSET(0)], '$.company') AS company,
-            t.created_at AS created_at,
-            t.deleted_at AS deleted_at,
-            JSON_EXTRACT_SCALAR(t.shipping_addresses[SAFE_OFFSET(0)], '$.email') AS email,
-            JSON_EXTRACT_SCALAR(t.shipping_addresses[SAFE_OFFSET(0)], '$.first_name') AS first_name,
-            ROW_NUMBER() OVER (PARTITION BY t.id ORDER BY t.updated_at DESC) = 1 AS is_most_recent_record,
-            t.tax_exempt AS is_tax_exempt,
-            JSON_EXTRACT_SCALAR(t.shipping_addresses[SAFE_OFFSET(0)], '$.last_name') AS last_name,
-            JSON_EXTRACT_SCALAR(t.address, '$.region') AS state,
-            t.username AS username,
-            JSON_EXTRACT_SCALAR(t.shipping_addresses[SAFE_OFFSET(0)], '$.vat_number') AS vat_number
+            id AS account_id,
+            cast(updated_at AS {{ dbt.type_timestamp() }}) AS account_updated_at,
+            NULL AS amount, 
+            NULL AS currency, 
+            has_past_due_invoice AS past_due,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1 AS is_most_recent_record
         FROM
-            {{ source('source_recurly', 'accounts') }} t
+            {{ source('source_recurly', 'accounts') }}
     )
     SELECT * FROM tmp
-    WHERE account_id IS NOT NULL 
+    WHERE account_id IS NOT NULL
 
 {% elif target.type == "postgres" %}
 
     WITH tmp AS (
         SELECT
-            t.id AS account_id,
-            t.updated_at AS updated_at,
-            t.address->>'city' AS account_city,
-            t.address->>'country' AS account_country,
-            t.address->>'postal_code' AS account_postal_code,
-            t.address->>'region' AS account_region,
-            t.bill_to AS bill_to,
-            t.cc_emails AS cc_emails,
-            t.code AS code,
-            t.shipping_addresses->0->>'company' AS company,
-            t.created_at AS created_at,
-            t.deleted_at AS deleted_at,
-            t.shipping_addresses->0->>'email' AS email,
-            t.shipping_addresses->0->>'first_name' AS first_name,
-            ROW_NUMBER() OVER (PARTITION BY t.id ORDER BY t.updated_at DESC) = 1 AS is_most_recent_record,
-            t.tax_exempt AS is_tax_exempt,
-            t.shipping_addresses->0->>'last_name' AS last_name,
-            t.address->>'region' AS state,
-            t.username AS username,
-            t.shipping_addresses->0->>'vat_number' AS vat_number
+            id AS account_id,
+            cast(updated_at AS {{ dbt.type_timestamp() }}) AS account_updated_at,
+            NULL AS amount, 
+            NULL AS currency,  
+            has_past_due_invoice AS past_due,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1 AS is_most_recent_record
         FROM
-            {{ source('source_recurly', 'accounts') }} t
+            {{ source('source_recurly', 'accounts') }}
     )
     SELECT * FROM tmp
-    WHERE account_id IS NOT NULL 
+    WHERE account_id IS NOT NULL
 
 {% endif %}
---
+++ connectors/source_recurly/models/fivetran_converter/transaction.sql
@@ -0,0 +1,154 @@
+{% if target.type == "snowflake" %}
+
+    WITH tmp AS (
+        SELECT
+            id AS transaction_id,
+            cast(collected_at AS {{ dbt.type_timestamp() }}) AS created_at,
+            account_id,
+            cast(amount AS {{ dbt.type_float() }}) AS amount,
+            NULL AS billing_city,
+            NULL AS billing_country,
+            NULL AS billing_first_name,
+            NULL AS billing_last_name,
+            NULL AS billing_phone,
+            NULL AS billing_postal_code,
+            NULL AS billing_region,
+            NULL AS billing_street_1,
+            NULL AS billing_street_2,
+            cast(collected_at AS {{ dbt.type_timestamp() }}) AS collected_at,
+            NULL AS collection_method,
+            currency,
+            message AS customer_message,
+            NULL AS customer_message_locale,
+            NULL AS gateway_approval_code,
+            message AS gateway_message,
+            NULL AS gateway_reference,
+            error_code AS gateway_response_code,
+            NULL AS gateway_response_time,
+            NULL AS gateway_response_values,
+            invoice_id,
+            CASE WHEN status = 'refunded' THEN TRUE ELSE FALSE END AS is_refunded,
+            CASE WHEN status = 'success' THEN TRUE ELSE FALSE END AS is_successful,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY collected_at DESC) = 1 AS is_most_recent_record,
+            origin,
+            NULL AS original_transaction_id,
+            gateway_id AS payment_gateway_id,
+            gateway AS payment_gateway_name,
+            NULL AS payment_gateway_type,
+            payment_method AS payment_method_object,
+            status,
+            NULL AS status_code,
+            message AS status_message,
+            type,
+            id AS uuid,
+            cast(voided_at AS {{ dbt.type_timestamp() }}) AS voided_at,
+            NULL AS voided_by_invoice_id
+        FROM
+            {{ source('source_recurly', 'transactions') }}
+    )
+    SELECT * FROM tmp
+    WHERE transaction_id IS NOT NULL
+
+{% elif target.type == "bigquery" %}
+
+    WITH tmp AS (
+        SELECT
+            id AS transaction_id,
+            cast(collected_at AS {{ dbt.type_timestamp() }}) AS created_at,
+            account_id,
+            cast(amount AS {{ dbt.type_float() }}) AS amount,
+            NULL AS billing_city,
+            NULL AS billing_country,
+            NULL AS billing_first_name,
+            NULL AS billing_last_name,
+            NULL AS billing_phone,
+            NULL AS billing_postal_code,
+            NULL AS billing_region,
+            NULL AS billing_street_1,
+            NULL AS billing_street_2,
+            cast(collected_at AS {{ dbt.type_timestamp() }}) AS collected_at,
+            NULL AS collection_method,
+            currency,
+            message AS customer_message,
+            NULL AS customer_message_locale,
+            NULL AS gateway_approval_code,
+            message AS gateway_message,
+            NULL AS gateway_reference,
+            error_code AS gateway_response_code,
+            NULL AS gateway_response_time,
+            NULL AS gateway_response_values,
+            invoice_id,
+            CASE WHEN status = 'refunded' THEN TRUE ELSE FALSE END AS is_refunded,
+            CASE WHEN status = 'success' THEN TRUE ELSE FALSE END AS is_successful,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY collected_at DESC) = 1 AS is_most_recent_record,
+            origin,
+            NULL AS original_transaction_id,
+            gateway_id AS payment_gateway_id,
+            gateway AS payment_gateway_name,
+            NULL AS payment_gateway_type,
+            payment_method AS payment_method_object,
+            status,
+            NULL AS status_code,
+            message AS status_message,
+            type,
+            id AS uuid,
+            cast(voided_at AS {{ dbt.type_timestamp() }}) AS voided_at,
+            NULL AS voided_by_invoice_id
+        FROM
+            {{ source('source_recurly', 'transactions') }}
+    )
+    SELECT * FROM tmp
+    WHERE transaction_id IS NOT NULL
+
+{% elif target.type == "postgres" %}
+
+    WITH tmp AS (
+        SELECT
+            id AS transaction_id,
+            cast(collected_at AS {{ dbt.type_timestamp() }}) AS created_at,
+            account_id,
+            cast(amount AS {{ dbt.type_float() }}) AS amount,
+            NULL AS billing_city,
+            NULL AS billing_country,
+            NULL AS billing_first_name,
+            NULL AS billing_last_name,
+            NULL AS billing_phone,
+            NULL AS billing_postal_code,
+            NULL AS billing_region,
+            NULL AS billing_street_1,
+            NULL AS billing_street_2,
+            cast(collected_at AS {{ dbt.type_timestamp() }}) AS collected_at,
+            NULL AS collection_method,
+            currency,
+            message AS customer_message,
+            NULL AS customer_message_locale,
+            NULL AS gateway_approval_code,
+            message AS gateway_message,
+            NULL AS gateway_reference,
+            error_code AS gateway_response_code,
+            NULL AS gateway_response_time,
+            NULL AS gateway_response_values,
+            invoice_id,
+            CASE WHEN status = 'refunded' THEN TRUE ELSE FALSE END AS is_refunded,
+            CASE WHEN status = 'success' THEN TRUE ELSE FALSE END AS is_successful,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY collected_at DESC) = 1 AS is_most_recent_record,
+            origin,
+            NULL AS original_transaction_id,
+            gateway_id AS payment_gateway_id,
+            gateway AS payment_gateway_name,
+            NULL AS payment_gateway_type,
+            payment_method AS payment_method_object,
+            status,
+            NULL AS status_code,
+            message AS status_message,
+            type,
+            id AS uuid,
+            cast(voided_at AS {{ dbt.type_timestamp() }}) AS voided_at,
+            NULL AS voided_by_invoice_id
+        FROM
+            {{ source('source_recurly', 'transactions') }}
+    )
+    SELECT * FROM tmp
+    WHERE transaction_id IS NOT NULL
+
+{% endif %}
