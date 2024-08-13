6286239e65b01fe5e6a95c24a3921c82e2495c24
Florian Chabert <65234487+FloDevelops@users.noreply.github.com>
2024-08-14 01:47:43 +0200

    feat: add unionned accounts

---
+++ connectors/source_facebook_marketing/models/staging/stg_facebook_ads__account_history.sql
@@ -0,0 +1,30 @@
+{% set sources_dict = var('airbyte')['facebook_ads']['sources']['ad_accounts'] %}
+{% set sources = convert_to_relations(sources_dict) %}
+
+with unionned as (
+{{
+  dbt_utils.union_relations(
+    relations=sources
+  )
+}}
+)
+
+,final as (
+  select
+    _dbt_source_relation as source_relation
+    ,cast(account_id as {{ dbt.type_bigint() }}) as account_id
+    ,_airbyte_extracted_at as _fivetran_synced
+    ,name as account_name
+    ,account_status
+    ,business_country_code
+    ,created_time as created_at
+    ,currency
+    ,timezone_name
+    ,case when account_id is null and _airbyte_extracted_at is null 
+      then row_number() over (partition by _dbt_source_relation order by _dbt_source_relation)
+    else row_number() over (partition by _dbt_source_relation, account_id order by _airbyte_extracted_at desc) end = 1 as is_most_recent_record
+  from unionned
+)
+
+select * from final
+
