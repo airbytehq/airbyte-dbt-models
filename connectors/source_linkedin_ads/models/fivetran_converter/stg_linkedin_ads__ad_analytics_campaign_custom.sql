4cdbd005525bb19328b92eeebd4d16b3fb9aedc5
Om Bhardwaj <115864495+ombhardwajj@users.noreply.github.com>
2024-08-15 12:24:42 +0530

    airbytehq#18 | Table 2 Transformation Complete:Fixed the sql file for the transformation of Table 2

--- connectors/source_linkedin_ads/models/fivetran_converter/stg_linkedin_ads__ad_analytics_campaign_custom.sql
+++ connectors/source_linkedin_ads/models/fivetran_converter/stg_linkedin_ads__ad_analytics_campaign_custom.sql
@@ -1,47 +1,34 @@
 {% if target.type == "snowflake" %}
 
-    {% set table_ad_campaign_analytics = schema ~ '.ad_campaign_analytics' %}
-    {% set table_campaigns = schema ~ '.campaigns' %}
-
     with tmp as (
         select
             ac.id as campaign_id,
             aca.clicks as clicks,
             aca.impressions as impressions,
-            aca.costInLocalCurrency as cost_in_local_currency,
-            aca.costInUsd as cost_in_usd,
-            aca.conversionValueInLocalCurrency as conversion_value_in_local_currency,
+            "aca"."costInLocalCurrency" as cost_in_local_currency,
+            "aca"."costInUsd" as cost_in_usd,
+            "aca"."conversionValueInLocalCurrency" as conversion_value_in_local_currency,
             {{ dbt.date_trunc('day', 'aca.start_date') }} as date_day
         from
-            {{ table_ad_campaign_analytics }} aca
-        join
-            {{ table_campaigns }} ac
-        on
-            aca.campaign_id = ac.id
+            {{ source('source_linkedin_ads', 'campaigns') }} as ac,  -- Cross join (implicit) with ad_campaign_analytics
+            {{ source('source_linkedin_ads', 'ad_campaign_analytics') }} as aca
     )
     select * from tmp
 
 {% elif target.type == "bigquery" %}
 
-    {% set schema = var('airbyte_schema', target.schema) %}
-    {% set table_ad_campaign_analytics = schema ~ '.ad_campaign_analytics' %}
-    {% set table_campaigns = schema ~ '.campaigns' %}
-
     with tmp as (
         select
             ac.id as campaign_id,
             aca.clicks as clicks,
             aca.impressions as impressions,
-            aca.costInLocalCurrency as cost_in_local_currency,
-            aca.costInUsd as cost_in_usd,
-            aca.conversionValueInLocalCurrency as conversion_value_in_local_currency,
+            "aca"."costInLocalCurrency" as cost_in_local_currency,
+            "aca"."costInUsd" as cost_in_usd,
+            "aca"."conversionValueInLocalCurrency" as conversion_value_in_local_currency,
             {{ dbt.date_trunc('day', 'aca.start_date') }} as date_day
         from
-            {{ table_ad_campaign_analytics }} aca
-        join
-            {{ table_campaigns }} ac
-        on
-            aca.campaign_id = ac.id
+            {{ source('source_linkedin_ads', 'campaigns') }} as ac,  -- Cross join (implicit) with ad_campaign_analytics
+            {{ source('source_linkedin_ads', 'ad_campaign_analytics') }} as aca
     )
     select * from tmp
 
@@ -52,16 +39,13 @@
             ac.id as campaign_id,
             aca.clicks as clicks,
             aca.impressions as impressions,
-            aca.costInLocalCurrency as cost_in_local_currency,
-            aca.costInUsd as cost_in_usd,
-            aca.conversionValueInLocalCurrency as conversion_value_in_local_currency,
+            "aca"."costInLocalCurrency" as cost_in_local_currency,
+            "aca"."costInUsd" as cost_in_usd,
+            "aca"."conversionValueInLocalCurrency" as conversion_value_in_local_currency,
             {{ dbt.date_trunc('day', 'aca.start_date') }} as date_day
         from
+            {{ source('source_linkedin_ads', 'campaigns') }} as ac,  -- Cross join (implicit) with ad_campaign_analytics
             {{ source('source_linkedin_ads', 'ad_campaign_analytics') }} as aca
-        join
-            {{ source('source_linkedin_ads', 'campaigns') }} as ac
-        on
-            aca.campaign_id = ac.id
     )
     select * from tmp
 
