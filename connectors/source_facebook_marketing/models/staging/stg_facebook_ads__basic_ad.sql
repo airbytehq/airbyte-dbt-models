e4cf9ef7f6a5ceb78dff077a4b187f588a41577e
Florian Chabert <65234487+FloDevelops@users.noreply.github.com>
2024-08-16 00:37:29 +0200

    feat: add basic ad staging

---
+++ connectors/source_facebook_marketing/models/staging/stg_facebook_ads__basic_ad.sql
@@ -0,0 +1,48 @@
+{% set sources_dict = var('airbyte')['facebook_ads']['sources']['ads_insights'] %}
+{% set sources = convert_to_relations(sources_dict) %}
+
+with unionned as (
+{{
+  dbt_utils.union_relations(
+    relations=sources
+  )
+}}
+)
+
+,final as (
+  select
+    _dbt_source_relation as source_relation
+    ,cast(ad_id as {{ dbt.type_bigint() }}) as ad_id
+    ,ad_name
+    ,adset_name as ad_set_name
+    ,date_start as date_day -- WIP: check data type
+    ,cast(account_id as {{ dbt.type_bigint() }}) as account_id
+    ,impressions
+    ,coalesce(inline_link_clicks,0) as clicks
+    ,spend
+
+    {# 
+        Reach and Frequency are not included in downstream models by default, though they are included in the staging model.
+        The below ensures that users can add Reach and Frequency to downstream models with the `basic_ad_passthrough_metrics` variable
+        while avoiding duplicate column errors.
+    #}
+    {%- set check = [] %}
+    {%- for field in var('airbyte')['facebook_ads']['basic_ad_passthrough_metrics'] -%}
+      {%- set field_name = field.alias|default(field.name)|lower %}
+      {% if field_name in ['reach', 'frequency'] %}
+        {% do check.append(field_name) %}
+      {% endif %}
+    {%- endfor %}
+
+    {%- for metric in ['reach', 'frequency'] -%}
+      {% if metric not in check %}
+        ,{{ metric }}
+      {% endif %}
+    {%- endfor %}
+
+    {{ fill_pass_through_columns(var('airbyte')['facebook_ads']['basic_ad_passthrough_metrics']) }}
+
+    from unionned
+)
+
+select * from final
