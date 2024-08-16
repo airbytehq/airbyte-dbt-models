7959e463ab248200674a9454e1ae24f9b816b03a
marcosmarxm <marcosmarxm@gmail.com>
2024-08-16 12:11:41 -0300

    fix account_balance_history and account_history

--- connectors/source_recurly/models/fivetran_converter/account_history.sql
+++ connectors/source_recurly/models/fivetran_converter/account_history.sql
@@ -4,10 +4,10 @@
         SELECT
             id AS account_id,
             cast(updated_at AS {{ dbt.type_timestamp() }}) AS updated_at,
-            NULL AS account_city,
-            NULL AS account_country,
-            NULL AS account_postal_code,
-            NULL AS account_region,
+            address->>'city' AS account_city,
+            address->>'country' AS account_country,
+            address->>'postal_code' AS account_postal_code,
+            address->>'region' AS account_region,
             bill_to,
             cc_emails,
             code,
@@ -34,10 +34,10 @@
         SELECT
             id AS account_id,
             cast(updated_at AS {{ dbt.type_timestamp() }}) AS updated_at,
-            NULL AS account_city, 
-            NULL AS account_country, 
-            NULL AS account_postal_code, 
-            NULL AS account_region, 
+            address:"city"::string AS account_city,
+            address:"country"::string AS account_country,
+            address:"postal_code"::string AS account_postal_code,
+            address:"region"::string AS account_region,
             bill_to,
             cc_emails,
             code,
@@ -64,10 +64,10 @@
         SELECT
             id AS account_id,
             cast(updated_at AS {{ dbt.type_timestamp() }}) AS updated_at,
-            NULL AS account_city,
-            NULL AS account_country,
-            NULL AS account_postal_code,
-            NULL AS account_region,
+            JSON_EXTRACT_SCALAR(address, '$.city') AS account_city
+            JSON_EXTRACT_SCALAR(address, '$.country') AS account_country
+            JSON_EXTRACT_SCALAR(address, '$.postal_code') AS account_postal_code
+            JSON_EXTRACT_SCALAR(address, '$.region') AS account_region
             bill_to,
             cc_emails,
             code,
