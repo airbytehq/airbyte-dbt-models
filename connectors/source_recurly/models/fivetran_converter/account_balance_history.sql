73fa85b3156000d906dc1d713818d5aebd51e2ec
marcosmarxm <marcosmarxm@gmail.com>
2024-08-16 12:11:41 -0300

    fix account_balance_history and account_history

--- connectors/source_recurly/models/fivetran_converter/account_balance_history.sql
+++ connectors/source_recurly/models/fivetran_converter/account_balance_history.sql
@@ -1,49 +1,13 @@
-{% if target.type == "postgres" %}
-
-    WITH tmp AS (
-        SELECT
-            id AS account_id,
-            cast(updated_at AS {{ dbt.type_timestamp() }}) AS account_updated_at,
-            NULL AS amount, 
-            NULL AS currency,  
-            has_past_due_invoice AS past_due,
-            ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1 AS is_most_recent_record
-        FROM
-            {{ source('source_recurly', 'accounts') }}
-    )
-    SELECT * FROM tmp
-    WHERE account_id IS NOT NULL
-
-{% elif target.type == "snowflake" %}
-
-    WITH tmp AS (
-        SELECT
-            id AS account_id,
-            cast(updated_at AS {{ dbt.type_timestamp() }}) AS account_updated_at,
-            NULL AS amount, 
-            NULL AS currency, 
-            has_past_due_invoice AS past_due,
-            ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1 AS is_most_recent_record
-        FROM
-            {{ source('source_recurly', 'accounts') }}
-    )
-    SELECT * FROM tmp
-    WHERE account_id IS NOT NULL
-
-{% elif target.type == "bigquery" %}
-
-    WITH tmp AS (
-        SELECT
-            id AS account_id,
-            cast(updated_at AS {{ dbt.type_timestamp() }}) AS account_updated_at,
-            NULL AS amount, 
-            NULL AS currency, 
-            has_past_due_invoice AS past_due,
-            ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1 AS is_most_recent_record
-        FROM
-            {{ source('source_recurly', 'accounts') }}
-    )
-    SELECT * FROM tmp
-    WHERE account_id IS NOT NULL
-
-{% endif %}
+with ab as (select
+  {{ fivetran_utils.json_extract('account', 'id') }} as account_id,
+  amount as amount,
+  currency as currency
+from {{source('source_recurly', 'transactions')}}
+)
+select
+    account_id,
+    currency,
+    sum(amount) as amount,
+    false as past_due
+from ab
+group by account_id, currency
