04ea47c4d551d9f0f9ddc37e15217d6ab0517ccc
btkcodedev <btk.codedev@gmail.com>
2024-08-19 21:29:38 +0530

    fix models

--- connectors/source_recurly/models/fivetran_converter/subscription_add_on_history.sql
+++ connectors/source_recurly/models/fivetran_converter/subscription_add_on_history.sql
@@ -1,17 +1,61 @@
-WITH tmp AS (
-    SELECT
-        id AS subscription_add_on_id,
-        updated_at,
-        created_at,
-        NULL AS expired_at,
-        NULL AS object,
-        plan_id AS plan_add_on_id,
-        cast(quantity AS INTEGER) AS quantity,
-        cast(subscription_id AS VARCHAR) AS subscription_id,
-        cast(unit_amount AS {{ dbt.type_float() }}) AS unit_amount,
-        cast(is_most_recent_record AS BOOLEAN) AS is_most_recent_record
-    FROM
-        {{ source('source_recurly', 'add_ons') }}
-)
-SELECT * FROM tmp
-WHERE subscription_add_on_id IS NOT NULL
+{% if target.type == "postgres" %}
+
+    WITH tmp AS (
+        SELECT
+            id AS subscription_add_on_id,
+            updated_at,
+            created_at,
+            NULL AS expired_at,
+            NULL AS object,
+            plan_id AS plan_add_on_id,
+            default_quantity AS quantity,
+            id AS subscription_id,
+            currencies->>unit_amount AS unit_amount,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1 AS is_most_recent_record
+        FROM
+            {{ source('source_recurly', 'add_ons') }}
+    )
+    SELECT * FROM tmp
+    WHERE subscription_add_on_id IS NOT NULL
+
+{% elif target.type == "snowflake" %}
+
+    WITH tmp AS (
+        SELECT
+            id AS subscription_add_on_id,
+            updated_at,
+            created_at,
+            NULL AS expired_at,
+            NULL AS object,
+            plan_id AS plan_add_on_id,
+            default_quantity AS quantity,
+            id AS subscription_id,
+            currencies:"unit_amount"::string AS unit_amount,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1 AS is_most_recent_record
+        FROM
+            {{ source('source_recurly', 'add_ons') }}
+    )
+    SELECT * FROM tmp
+    WHERE subscription_add_on_id IS NOT NULL
+
+{% elif target.type == "bigquery" %}
+
+    WITH tmp AS (
+        SELECT
+            id AS subscription_add_on_id,
+            updated_at,
+            created_at,
+            NULL AS expired_at,
+            NULL AS object,
+            plan_id AS plan_add_on_id,
+            default_quantity AS quantity,
+            id AS subscription_id,
+            JSON_EXTRACT_SCALAR(currencies, '$.unit_amount') AS unit_amount,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1 AS is_most_recent_record
+        FROM
+            {{ source('source_recurly', 'add_ons') }}
+    )
+    SELECT * FROM tmp
+    WHERE subscription_add_on_id IS NOT NULL
+
+{% endif %}
