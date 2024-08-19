fa4dc33a9345d7605b9fa46d18c7953b9205d297
btkcodedev <btk.codedev@gmail.com>
2024-08-19 18:48:41 +0530

    fix schemas

--- connectors/source_recurly/models/fivetran_converter/account_note_history.sql
+++ connectors/source_recurly/models/fivetran_converter/account_note_history.sql
@@ -1,58 +1,17 @@
-{% if target.type == "postgres" %}
+WITH tmp AS (
+    SELECT
+        id AS account_note_id,
+        account_id,
+        updated_at AS account_updated_at,
+        cast(created_at AS {{ dbt.type_timestamp() }}) AS created_at,
+        message,
+        object,
+        created_by AS user_email,
+        created_by AS user_id,
+        ROW_NUMBER() OVER (PARTITION BY id ORDER BY created_at DESC) = 1 AS is_most_recent_record
+    FROM
+        {{ source('source_recurly', 'account_notes') }}
+)
+SELECT * FROM tmp
+WHERE account_note_id IS NOT NULL
 
-    WITH tmp AS (
-        SELECT
-            id AS account_note_id,
-            account_id,
-            NULL AS account_updated_at,
-            cast(created_at AS {{ dbt.type_timestamp() }}) AS created_at,
-            message,
-            object,
-            NULL AS user_email,
-            NULL AS user_id,
-            ROW_NUMBER() OVER (PARTITION BY id ORDER BY created_at DESC) = 1 AS is_most_recent_record
-        FROM
-            {{ source('source_recurly', 'account_notes') }}
-    )
-    SELECT * FROM tmp
-    WHERE account_note_id IS NOT NULL
-
-{% elif target.type == "snowflake" %}
-
-    WITH tmp AS (
-        SELECT
-            id AS account_note_id,
-            account_id,
-            NULL AS account_updated_at,
-            cast(created_at AS {{ dbt.type_timestamp() }}) AS created_at,
-            message,
-            object,
-            NULL AS user_email,
-            NULL AS user_id, 
-            ROW_NUMBER() OVER (PARTITION BY id ORDER BY created_at DESC) = 1 AS is_most_recent_record
-        FROM
-            {{ source('source_recurly', 'account_notes') }}
-    )
-    SELECT * FROM tmp
-    WHERE account_note_id IS NOT NULL
-
-{% elif target.type == "bigquery" %}
-
-    WITH tmp AS (
-        SELECT
-            id AS account_note_id,
-            account_id,
-            NULL AS account_updated_at,
-            cast(created_at AS {{ dbt.type_timestamp() }}) AS created_at,
-            message,
-            object,
-            NULL AS user_email,
-            NULL AS user_id,
-            ROW_NUMBER() OVER (PARTITION BY id ORDER BY created_at DESC) = 1 AS is_most_recent_record
-        FROM
-            {{ source('source_recurly', 'account_notes') }}
-    )
-    SELECT * FROM tmp
-    WHERE account_note_id IS NOT NULL
-
-{% endif %}
