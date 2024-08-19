299f31d2aa94817564654ed9dcf9cf35c6086f30
btkcodedev <btk.codedev@gmail.com>
2024-08-19 19:37:36 +0530

    fix models

--- connectors/source_recurly/models/fivetran_converter/account_note_history.sql
+++ connectors/source_recurly/models/fivetran_converter/account_note_history.sql
@@ -1,17 +1,58 @@
-WITH tmp AS (
-    SELECT
-        id AS account_note_id,
-        account_id,
-        updated_at AS account_updated_at,
-        cast(created_at AS {{ dbt.type_timestamp() }}) AS created_at,
-        message,
-        object,
-        created_by AS user_email,
-        created_by AS user_id,
-        ROW_NUMBER() OVER (PARTITION BY id ORDER BY created_at DESC) = 1 AS is_most_recent_record
-    FROM
-        {{ source('source_recurly', 'account_notes') }}
-)
-SELECT * FROM tmp
-WHERE account_note_id IS NOT NULL
+{% if target.type == "postgres" %}
 
+    WITH tmp AS (
+        SELECT
+            id AS account_note_id,
+            account_id,
+            user->>'created_at' AS account_updated_at,
+            cast(user->>'created_at' AS {{ dbt.type_timestamp() }}) AS created_at,
+            message,
+            object,
+            user->>'email' AS user_email,
+            user->>'id' AS user_id,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY created_at DESC) = 1 AS is_most_recent_record
+        FROM
+            {{ source('source_recurly', 'account_notes') }}
+    )
+    SELECT * FROM tmp
+    WHERE account_note_id IS NOT NULL
+
+{% elif target.type == "snowflake" %}
+
+    WITH tmp AS (
+        SELECT
+            id AS account_note_id,
+            account_id,
+            user:"created_at"::string AS account_updated_at,
+            cast(user:"created_at"::string AS {{ dbt.type_timestamp() }}) AS created_at,
+            message,
+            object,
+            user:"email"::string AS user_email,
+            user:"id"::string AS user_id,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY created_at DESC) = 1 AS is_most_recent_record
+        FROM
+            {{ source('source_recurly', 'account_notes') }}
+    )
+    SELECT * FROM tmp
+    WHERE account_note_id IS NOT NULL
+
+{% elif target.type == "bigquery" %}
+
+    WITH tmp AS (
+        SELECT
+            id AS account_note_id,
+            account_id,
+            JSON_EXTRACT_SCALAR(user, '$.created_at') AS account_updated_at,
+            cast(JSON_EXTRACT_SCALAR(user, '$.created_at') AS {{ dbt.type_timestamp() }}) AS created_at,
+            message,
+            object,
+            JSON_EXTRACT_SCALAR(user, '$.email') AS user_email,
+            JSON_EXTRACT_SCALAR(user, '$.id') AS user_id,
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY created_at DESC) = 1 AS is_most_recent_record
+        FROM
+            {{ source('source_recurly', 'account_notes') }}
+    )
+    SELECT * FROM tmp
+    WHERE account_note_id IS NOT NULL
+
+{% endif %}
--- connectors/source_recurly/models/fivetran_converter/invoice_subscription_history.sql
+++ connectors/source_recurly/models/fivetran_converter/invoice_subscription_history.sql
@@ -1,44 +1,11 @@
-
-{% if target.type == "postgres" %}
-
-    WITH tmp AS (
-        SELECT
-            NULL AS invoice_id,
-            NULL AS invoice_updated_at,
-            NULL AS subscription_id,
-            ROW_NUMBER() OVER (PARTITION BY id ORDER BY invoice_updated_at DESC) = 1 AS is_most_recent_record
-        FROM
-            {{ source('source_recurly', 'invoices') }}
-    )
-    SELECT * FROM tmp
-    WHERE invoice_id IS NOT NULL
-
-{% elif target.type == "snowflake" %}
-
-    WITH tmp AS (
-        SELECT
-            NULL AS invoice_id,
-            NULL AS invoice_updated_at,
-            NULL AS subscription_id,
-            ROW_NUMBER() OVER (PARTITION BY id ORDER BY invoice_updated_at DESC) = 1 AS is_most_recent_record
-        FROM
-            {{ source('source_recurly', 'invoices') }}
-    )
-    SELECT * FROM tmp
-    WHERE invoice_id IS NOT NULL
-
-{% elif target.type == "bigquery" %}
-
-    WITH tmp AS (
-        SELECT
-            NULL AS invoice_id,
-            NULL AS invoice_updated_at,
-            NULL AS subscription_id,
-            ROW_NUMBER() OVER (PARTITION BY id ORDER BY invoice_updated_at DESC) = 1 AS is_most_recent_record
-        FROM
-            {{ source('source_recurly', 'invoices') }}
-    )
-    SELECT * FROM tmp
-    WHERE invoice_id IS NOT NULL
-
-{% endif %}
+WITH tmp AS (
+    SELECT
+        id AS invoice_id,
+        cast(updated_at AS TIMESTAMP) AS invoice_updated_at,
+        cast(subscription_ids[0]) AS subscription_id,
+        ROW_NUMBER() OVER (PARTITION BY id ORDER BY invoice_updated_at DESC) = 1 AS is_most_recent_record
+    FROM
+        {{ source('source_recurly', 'invoices') }}
+)
+SELECT * FROM tmp
+WHERE invoice_id IS NOT NULL
