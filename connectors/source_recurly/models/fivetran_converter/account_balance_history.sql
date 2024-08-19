3148346a82b6b5a8d80025f66915fc28e8cc1a37
btkcodedev <btk.codedev@gmail.com>
2024-08-19 21:59:05 +0530

    fix account_balance_history

--- connectors/source_recurly/models/fivetran_converter/account_balance_history.sql
+++ connectors/source_recurly/models/fivetran_converter/account_balance_history.sql
@@ -1,52 +1,52 @@
 {% if target.type == "postgres" %}
 
-    WITH tmp AS (
-        SELECT
-            account->>'id' AS account_id,
-            MAX(updated_at) AS account_updated_at,
-            SUM(amount) AS amount, 
-            currency,
-            false AS past_due,
-            ROW_NUMBER() OVER (PARTITION BY account->>'id', currency ORDER BY MAX(updated_at) DESC) AS is_most_recent_record
-        FROM
-            {{ source('source_recurly', 'transactions') }}
-        GROUP BY account->>'id', currency
-    )
-    SELECT * FROM tmp
-    WHERE account_id IS NOT NULL
+  WITH ab AS (
+      SELECT
+          account->>'id' AS account_id,
+          amount AS amount,
+          currency AS currency
+      FROM {{ source('source_recurly', 'transactions') }}
+  )
+  SELECT
+      account_id,
+      currency,
+      SUM(amount) AS amount,
+      FALSE AS past_due
+  FROM ab
+  GROUP BY account_id, currency
 
 {% elif target.type == "snowflake" %}
 
-    WITH tmp AS (
-        SELECT
-            account:id AS account_id,
-            MAX(updated_at) AS account_updated_at,
-            SUM(amount) AS amount, 
-            currency,
-            false AS past_due,
-            ROW_NUMBER() OVER (PARTITION BY account:id, currency ORDER BY MAX(updated_at) DESC) AS is_most_recent_record
-        FROM
-            {{ source('source_recurly', 'transactions') }}
-        GROUP BY account:id, currency
-    )
-    SELECT * FROM tmp
-    WHERE account_id IS NOT NULL
+  WITH ab AS (
+      SELECT
+          account:id AS account_id,
+          amount AS amount,
+          currency AS currency
+      FROM {{ source('source_recurly', 'transactions') }}
+  )
+  SELECT
+      account_id,
+      currency,
+      SUM(amount) AS amount,
+      FALSE AS past_due
+  FROM ab
+  GROUP BY account_id, currency
 
 {% elif target.type == "bigquery" %}
 
-    WITH tmp AS (
-        SELECT
-            JSON_EXTRACT_SCALAR(account, '$.id') AS account_id,
-            MAX(updated_at) AS account_updated_at,
-            SUM(amount) AS amount, 
-            currency,
-            false AS past_due,
-            ROW_NUMBER() OVER (PARTITION BY JSON_EXTRACT_SCALAR(account, '$.id'), currency ORDER BY MAX(updated_at) DESC) AS is_most_recent_record
-        FROM
-            {{ source('source_recurly', 'transactions') }}
-        GROUP BY JSON_EXTRACT_SCALAR(account, '$.id'), currency
-    )
-    SELECT * FROM tmp
-    WHERE account_id IS NOT NULL
+  WITH ab AS (
+      SELECT
+          JSON_EXTRACT_SCALAR(account, '$.id') AS account_id,
+          amount AS amount,
+          currency AS currency
+      FROM {{ source('source_recurly', 'transactions') }}
+  )
+  SELECT
+      account_id,
+      currency,
+      SUM(amount) AS amount,
+      FALSE AS past_due
+  FROM ab
+  GROUP BY account_id, currency
 
 {% endif %}
