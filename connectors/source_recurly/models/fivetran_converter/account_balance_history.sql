e953ea723292df0762b3f14a56e57b348bb6797a
btkcodedev <btk.codedev@gmail.com>
2024-08-19 21:45:16 +0530

    fix account_balance_history

--- connectors/source_recurly/models/fivetran_converter/account_balance_history.sql
+++ connectors/source_recurly/models/fivetran_converter/account_balance_history.sql
@@ -3,11 +3,11 @@
     WITH tmp AS (
         SELECT
             account->>id AS account_id,
-            updated_at AS account_updated_at,
-            sum(amount) as amount, 
+            MAX(updated_at) AS account_updated_at,
+            SUM(amount) as amount, 
             currency,
             false AS past_due,
-            ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1 AS is_most_recent_record
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY MAX(updated_at) DESC) = 1 AS is_most_recent_record
         FROM
             {{ source('source_recurly', 'transactions') }}
         GROUP BY account_id, currency
@@ -20,11 +20,11 @@
     WITH tmp AS (
         SELECT
             account:id AS account_id,
-            updated_at AS account_updated_at,
-            sum(amount) as amount, 
+            MAX(updated_at) AS account_updated_at,
+            SUM(amount) as amount, 
             currency,
             false AS past_due,
-            ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1 AS is_most_recent_record
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY MAX(updated_at) DESC) = 1 AS is_most_recent_record
         FROM
             {{ source('source_recurly', 'transactions') }}
         GROUP BY account_id, currency
@@ -37,11 +37,11 @@
     WITH tmp AS (
         SELECT
             JSON_EXTRACT_SCALAR(account, '$.id') AS account_id,
-            updated_at AS account_updated_at,
-            sum(amount) as amount, 
+            MAX(updated_at) AS account_updated_at,
+            SUM(amount) as amount, 
             currency,
             false AS past_due,
-            ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1 AS is_most_recent_record
+            ROW_NUMBER() OVER (PARTITION BY id ORDER BY MAX(updated_at) DESC) = 1 AS is_most_recent_record
         FROM
             {{ source('source_recurly', 'transactions') }}
         GROUP BY account_id, currency
