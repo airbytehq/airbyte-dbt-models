abd87352186d8fe4790b1b73cce279d3737ffcef
btkcodedev <btk.codedev@gmail.com>
2024-08-19 21:29:38 +0530

    fix models

--- connectors/source_recurly/models/fivetran_converter/invoice_coupon_redemption_history.sql
+++ connectors/source_recurly/models/fivetran_converter/invoice_coupon_redemption_history.sql
@@ -1,6 +1,6 @@
 WITH tmp AS (
     SELECT
-        NULL AS coupon_redemption_id,
+        id AS coupon_redemption_id,
         id AS invoice_id,
         updated_at AS invoice_updated_at,
         ROW_NUMBER() OVER (PARTITION BY id ORDER BY updated_at DESC) = 1 AS is_most_recent_record
