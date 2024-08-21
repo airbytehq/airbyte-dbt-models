ef7bf043c1364a41f376205086dc15a9427837a7
Florian Chabert <65234487+FloDevelops@users.noreply.github.com>
2024-08-22 01:54:59 +0200

    chore: remove unused documentation and macros

--- connectors/source_facebook_marketing/macros/convert_to_relations.sql
+++
@@ -1,8 +0,0 @@
-{% macro convert_to_relations(source_list) %}
-  {% set relations = [] %}
-  {% for source_item in source_list %}
-    {% set relation = source(source_item['name'], source_item['table']) %}
-    {% do relations.append(relation) %}
-  {% endfor %}
-  {{ return(relations) }}
-{% endmacro %}
