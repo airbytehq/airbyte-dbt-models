72d2de28f8dfb3ee65cd566fdeba271fda455df3
btkcodedev <btk.codedev@gmail.com>
2024-08-25 22:52:25 +0530

    fix other models

--- connectors/source_twilio/models/fivetran_converter/address.sql
+++ connectors/source_twilio/models/fivetran_converter/address.sql
@@ -1,67 +1,19 @@
-{% if target.type == "snowflake" %}
-
-    SELECT
-        {{ dbt.current_timestamp() }} AS _fivetran_synced,
-        account_sid AS account_id,
-        city,
-        cast(date_created AS {{ dbt.type_timestamp() }}) AS created_at,
-        customer_name,
-        emergency_enabled,
-        friendly_name,
-        cast(sid AS {{ dbt.type_string() }}) AS address_id,
-        iso_country,
-        postal_code,
-        region,
-        street,
-        cast(date_updated AS {{ dbt.type_timestamp() }}) AS updated_at,
-        validated,
-        verified
-    FROM
-        {{ source('source_twilio', 'addresses') }}
-    WHERE sid IS NOT NULL
-
-{% elif target.type == "bigquery" %}
-
-    SELECT
-        {{ dbt.current_timestamp() }} AS _fivetran_synced,
-        account_sid AS account_id,
-        city,
-        cast(date_created AS {{ dbt.type_timestamp() }}) AS created_at,
-        customer_name,
-        emergency_enabled,
-        friendly_name,
-        cast(sid AS {{ dbt.type_string() }}) AS address_id,
-        iso_country,
-        postal_code,
-        region,
-        street,
-        cast(date_updated AS {{ dbt.type_timestamp() }}) AS updated_at,
-        validated,
-        verified
-    FROM
-        {{ source('source_twilio', 'addresses') }}
-    WHERE sid IS NOT NULL
-
-{% elif target.type == "postgres" %}
-
-    SELECT
-        {{ dbt.current_timestamp() }} AS _fivetran_synced,
-        account_sid AS account_id,
-        city,
-        cast(date_created AS {{ dbt.type_timestamp() }}) AS created_at,
-        customer_name,
-        emergency_enabled,
-        friendly_name,
-        cast(sid AS {{ dbt.type_string() }}) AS address_id,
-        iso_country,
-        postal_code,
-        region,
-        street,
-        cast(date_updated AS {{ dbt.type_timestamp() }}) AS updated_at,
-        validated,
-        verified
-    FROM
-        {{ source('source_twilio', 'addresses') }}
-    WHERE sid IS NOT NULL
-
-{% endif %}
+SELECT
+    {{ dbt.current_timestamp() }} AS _fivetran_synced,
+    account_sid AS account_id,
+    city,
+    cast(date_created AS {{ dbt.type_timestamp() }}) AS created_at,
+    customer_name,
+    emergency_enabled,
+    friendly_name,
+    cast(sid AS {{ dbt.type_string() }}) AS address_id,
+    iso_country,
+    postal_code,
+    region,
+    street,
+    cast(date_updated AS {{ dbt.type_timestamp() }}) AS updated_at,
+    validated,
+    verified
+FROM
+    {{ source('source_twilio', 'addresses') }}
+WHERE sid IS NOT NULL
