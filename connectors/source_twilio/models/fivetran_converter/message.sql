6fb279a8eb754faeed8d25998b2c256dceba581f
btkcodedev <btk.codedev@gmail.com>
2024-08-25 22:38:10 +0530

    message model

---
+++ connectors/source_twilio/models/fivetran_converter/message.sql
@@ -0,0 +1,85 @@
+{% if target.type == "snowflake" %}
+
+    WITH tmp AS (
+        SELECT
+            {{ dbt.current_timestamp() }} AS _fivetran_synced,
+            account_sid AS account_id,
+            body,
+            cast(date_created AS {{ dbt.type_timestamp() }}) AS created_at,
+            cast(date_sent AS {{ dbt.type_timestamp() }}) AS timestamp_sent,
+            direction,
+            error_code,
+            error_message,
+            "from" AS message_from,
+            sid AS message_id,
+            cast(messaging_service_sid AS {{ dbt.type_string() }}) AS messaging_service_id,
+            cast({{ twilio_source.remove_non_numeric_chars('num_media') }} AS {{ dbt.type_float() }}) AS num_media,
+            cast({{ twilio_source.remove_non_numeric_chars('num_segments') }} AS {{ dbt.type_float() }}) AS num_segments,
+            cast({{ twilio_source.remove_non_numeric_chars('price') }} AS {{ dbt.type_float() }}) AS price,
+            price_unit,
+            status,
+            "to" AS message_to,
+            cast(date_updated AS {{ dbt.type_timestamp() }}) AS updated_at
+        FROM
+            {{ source('source_twilio', 'messages') }}
+    )
+    SELECT * FROM tmp
+    WHERE message_id IS NOT NULL
+
+{% elif target.type == "bigquery" %}
+
+    WITH tmp AS (
+        SELECT
+            {{ dbt.current_timestamp() }} AS _fivetran_synced,
+            account_sid AS account_id,
+            body,
+            cast(date_created AS {{ dbt.type_timestamp() }}) AS created_at,
+            cast(date_sent AS {{ dbt.type_timestamp() }}) AS timestamp_sent,
+            direction,
+            error_code,
+            error_message,
+            `from` AS message_from,
+            sid AS message_id,
+            cast(messaging_service_sid AS {{ dbt.type_string() }}) AS messaging_service_id,
+            cast({{ twilio_source.remove_non_numeric_chars('num_media') }} AS {{ dbt.type_float() }}) AS num_media,
+            cast({{ twilio_source.remove_non_numeric_chars('num_segments') }} AS {{ dbt.type_float() }}) AS num_segments,
+            cast({{ twilio_source.remove_non_numeric_chars('price') }} AS {{ dbt.type_float() }}) AS price,
+            price_unit,
+            status,
+            `to` AS message_to,
+            cast(date_updated AS {{ dbt.type_timestamp() }}) AS updated_at
+        FROM
+            {{ source('source_twilio', 'messages') }}
+    )
+    SELECT * FROM tmp
+    WHERE message_id IS NOT NULL
+
+{% elif target.type == "postgres" %}
+
+    WITH tmp AS (
+        SELECT
+            {{ dbt.current_timestamp() }} AS _fivetran_synced,
+            account_sid AS account_id,
+            body,
+            cast(date_created AS {{ dbt.type_timestamp() }}) AS created_at,
+            cast(date_sent AS {{ dbt.type_timestamp() }}) AS timestamp_sent,
+            direction,
+            error_code,
+            error_message,
+            "from" AS message_from,
+            sid AS message_id,
+            cast(messaging_service_sid AS {{ dbt.type_string() }}) AS messaging_service_id,
+            cast({{ twilio_source.remove_non_numeric_chars('num_media') }} AS {{ dbt.type_float() }}) AS num_media,
+            cast({{ twilio_source.remove_non_numeric_chars('num_segments') }} AS {{ dbt.type_float() }}) AS num_segments,
+            cast({{ twilio_source.remove_non_numeric_chars('price') }} AS {{ dbt.type_float() }}) AS price,
+            price_unit,
+            status,
+            "to" AS message_to,
+            cast(date_updated AS {{ dbt.type_timestamp() }}) AS updated_at
+        FROM
+            {{ source('source_twilio', 'messages') }}
+    )
+    SELECT * FROM tmp
+    WHERE message_id IS NOT NULL
+
+{% endif %}
