0a779da140628c2035b8a6b82f6f26af5819df6b
marcosmarxm <marcosmarxm@gmail.com>
2024-08-28 15:09:09 -0300

    fix queries in event sql

--- connectors/source_klaviyo/models/fivetran_converter/event.sql
+++ connectors/source_klaviyo/models/fivetran_converter/event.sql
@@ -1,12 +1,9 @@
 {% if target.type == "snowflake" %}
 
     SELECT
-        -- included:0:relationships:flow-message-variation:data:id::STRING AS variation,
-        -- included:0:relationships:flow:campaign:id::STRING AS campaign_id,
-
         attributes:event_properties:"$flow"::STRING AS flow_id,
         attributes:event_properties:"$message"::STRING AS flow_message_id,
-        DATE_FROM_UNIXTIME(CAST(attributes:timestamp AS NUMBER)) AS timestamp,
+        TO_TIMESTAMP(CAST(attributes:timestamp AS NUMBER)) AS timestamp,
         attributes:datetime::STRING AS datetime,
         id,
         relationships:metric:data:id::STRING AS metric_id,
@@ -19,18 +16,16 @@
 {% elif target.type == "bigquery" %}
 
     SELECT
-        -- JSON_EXTRACT_SCALAR(included, '$[0].relationships.flow-message-variation.data.id') AS variation,
-        -- JSON_EXTRACT_SCALAR(included, '$[0].relationships.flow.campaign.id') AS campaign_id,
 
-        JSON_EXTRACT_SCALAR(attributes, '$.event_properties.$flow') AS flow_id,
-        JSON_EXTRACT_SCALAR(attributes, '$.event_properties.$message') AS flow_message_id,
-        TIMESTAMP_SECONDS(CAST(JSON_EXTRACT_SCALAR(attributes, '$.timestamp') AS INT64)) AS timestamp,
-        JSON_EXTRACT_SCALAR(attributes, '$.datetime') AS datetime,
+        JSON_VALUE(attributes, '$.event_properties."$flow"') AS flow_id,
+        JSON_VALUE(attributes, '$.event_properties."$message"') AS flow_message_id,
+        TIMESTAMP_SECONDS(CAST(JSON_VALUE(attributes, '$.timestamp') AS INT64)) AS timestamp,
+        JSON_VALUE(attributes, '$.datetime') AS datetime,
         id,
-        JSON_EXTRACT_SCALAR(relationships, '$.metric.data.id') AS metric_id,
-        JSON_EXTRACT_SCALAR(relationships, '$.profile.data.id') AS person_id,
+        JSON_VALUE(relationships, '$.metric.data.id') AS metric_id,
+        JSON_VALUE(relationships, '$.profile.data.id') AS person_id,
         type,
-        JSON_EXTRACT_SCALAR(attributes, '$.uuid') AS uuid
+        JSON_VALUE(attributes, '$.uuid') AS uuid
     FROM
         {{ source('source_klaviyo', 'events') }}
 
