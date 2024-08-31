69b5a31eba2e87e1f1d7e8802261fc8e4ac48705
marcosmarxm <marcosmarxm@gmail.com>
2024-08-31 10:11:32 -0300

    fix account history

--- connectors/source_twilio/models/fivetran_converter/account_history.sql
+++ connectors/source_twilio/models/fivetran_converter/account_history.sql
@@ -1,16 +1,12 @@
-WITH tmp AS (
-    SELECT
-        {{ dbt.current_timestamp() }} AS _fivetran_synced,
-        cast(date_created AS {{ dbt.type_timestamp() }}) AS created_at,
-        friendly_name,
-        sid AS account_id,
-        owner_account_sid AS owner_account_id,
-        status,
-        type,
-        cast(date_updated AS {{ dbt.type_timestamp() }}) AS updated_at,
-        ROW_NUMBER() OVER (PARTITION BY sid ORDER BY date_updated DESC) = 1 AS is_most_recent_record
-    FROM
-        {{ source('source_twilio', 'accounts') }}
-)
-SELECT * FROM tmp
-WHERE account_id IS NOT NULL
+SELECT
+    {{ dbt.current_timestamp() }} AS _fivetran_synced,
+    cast(date_created AS {{ dbt.type_timestamp() }}) AS created_at,
+    friendly_name,
+    sid AS id,
+    owner_account_sid AS owner_account_id,
+    status,
+    type,
+    cast(date_updated AS {{ dbt.type_timestamp() }}) AS updated_at
+FROM
+    {{ source('source_twilio', 'accounts') }}
+
