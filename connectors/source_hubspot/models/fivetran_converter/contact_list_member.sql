9104813271c5167c0434dc4ea738801cfdfb5926
btkcodedev <btk.codedev@gmail.com>
2024-09-08 23:48:50 +0530

    EOF

--- connectors/source_hubspot/models/fivetran_converter/contact_list_member.sql
+++ connectors/source_hubspot/models/fivetran_converter/contact_list_member.sql
@@ -2,6 +2,6 @@ SELECT
     'FALSE' AS is_contact_list_member_deleted,
     {{ dbt.current_timestamp() }} AS _fivetran_synced,
     NULL AS contact_id,
-    ROW_NUMBER() OVER() AS contact_list_id
+    ROW_NUMBER() OVER (ORDER BY {{ dbt.current_timestamp() }} DESC) = 1 AS contact_list_id
 FROM
     {{ source('source_hubspot', 'contacts_list_memberships') }}
