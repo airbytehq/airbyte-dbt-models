1320cf1997e068007ee8ff26e6694fd452a05c63
btkcodedev <btk.codedev@gmail.com>
2024-09-08 20:38:13 +0530

    fix models

---
+++ connectors/source_hubspot/models/fivetran_converter/contact_merged_audit.sql
@@ -0,0 +1,49 @@
+{% if target.type == "postgres" %}
+
+SELECT
+    {{ dbt.current_timestamp() }} AS fivetran_synced,
+    "user-id" AS user_id,
+    vid_to_merge AS contact_id,
+    entity_id,
+    first_name,
+    last_name,
+    num_properties_moved,
+    CAST(timestamp_at AS {{ dbt.type_timestamp() }}) AS timestamp_at,
+    "user-id" AS original_user_id,
+    vid_to_merge
+FROM 
+    {{ source('source_hubspot', 'contacts_merged_audit') }}
+
+{% elif target.type == "snowflake" %}
+
+SELECT
+    {{ dbt.current_timestamp() }} AS fivetran_synced,
+    "user-id"::STRING AS user_id,
+    vid_to_merge AS contact_id,
+    entity_id,
+    first_name,
+    last_name,
+    num_properties_moved,
+    CAST(timestamp_at AS {{ dbt.type_timestamp() }}) AS timestamp_at,
+    "user-id"::STRING AS original_user_id,
+    vid_to_merge
+FROM 
+    {{ source('source_hubspot', 'contacts_merged_audit') }}
+
+{% elif target.type == "bigquery" %}
+
+SELECT
+    {{ dbt.current_timestamp() }} AS fivetran_synced,
+    `user-id` AS user_id,
+    vid_to_merge AS contact_id,
+    entity_id,
+    first_name,
+    last_name,
+    num_properties_moved,
+    CAST(timestamp_at AS {{ dbt.type_timestamp() }}) AS timestamp_at,
+    `user-id` AS original_user_id,
+    vid_to_merge
+FROM 
+    {{ source('source_hubspot', 'contacts_merged_audit') }}
+
+{% endif %}
