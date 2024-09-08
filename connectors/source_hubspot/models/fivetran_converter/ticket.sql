02cb7f807ef261f4bfb6563d4dd849fb88e6dc86
btkcodedev <btk.codedev@gmail.com>
2024-09-08 19:03:20 +0530

    fix models, add models

---
+++ connectors/source_hubspot/models/fivetran_converter/ticket.sql
@@ -0,0 +1,16 @@
+SELECT
+    id AS ticket_id,
+    'FALSE' AS is_ticket_deleted,
+    {{ dbt.type_timestamp() }} AS _fivetran_synced,
+    properties_closed_date AS closed_date,
+    properties_createdate AS created_date,
+    properties_first_agent_reply_date AS first_agent_reply_at,
+    properties_hs_pipeline AS ticket_pipeline_id,
+    properties_hs_pipeline_stage AS ticket_pipeline_stage_id,
+    properties_hs_ticket_category AS ticket_category,
+    properties_hs_ticket_priority AS ticket_priority,
+    properties_hubspot_owner_id AS owner_id,
+    properties_subject AS ticket_subject,
+    properties_content AS ticket_content
+FROM
+    {{ source('source_hubspot', 'tickets') }}
---
+++ connectors/source_hubspot/models/fivetran_converter/ticket_pipeline.sql
@@ -0,0 +1,11 @@
+SELECT
+    CAST(id AS {{ dbt.type_int() }}) AS ticket_pipeline_id,
+    'FALSE' AS is_ticket_pipeline_deleted,
+    {{ dbt.type_timestamp() }} AS _fivetran_synced,
+    NOT archived AS is_active,
+    displayOrder AS display_order,
+    label AS pipeline_label,
+    object_type_id,
+    createdAt AS ticket_pipeline_created_at,
+    updatedAt AS ticket_pipeline_updated_at
+FROM {{ source('source_hubspot', 'ticket_pipelines') }}
