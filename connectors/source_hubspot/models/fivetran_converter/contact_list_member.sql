1e4f2effb1a4738cb3d551f28d496a7a9d537694
btkcodedev <btk.codedev@gmail.com>
2024-09-08 23:11:15 +0530

    fix

--- connectors/source_hubspot/models/fivetran_converter/contact_list_member.sql
+++ connectors/source_hubspot/models/fivetran_converter/contact_list_member.sql
@@ -1,7 +1,7 @@
 SELECT
     'FALSE' AS is_contact_list_member_deleted,
     {{ dbt.current_timestamp() }} AS _fivetran_synced
-    vid AS contact_id,
+    NULL AS contact_id,
     static_list_id AS contact_list_id
 FROM
     {{ source('source_hubspot', 'contacts_list_memberships') }}
